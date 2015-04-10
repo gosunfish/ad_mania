7b2df83fcbfb3dc678db5bd9687a2447293d6323
Carolyn Evans <carolyn.evans@enom.com>
2015-04-09 20:48:42 -0700

    Initial checkin.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='ad_mania',
+    version = '0.1.0',
+    author='Carolyn Evans',
+    author_email='gosunfish@comcast.net',
+    packages = 'ad_mania' 'test',
+    url='https://github.com/gosunfish/ad_mania',
+    license='MIT',
+    description='Ad Server',
+    long_description=open('README.rst').read(),
+    install_requires=[],)
+
+
