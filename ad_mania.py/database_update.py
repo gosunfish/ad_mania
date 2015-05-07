f8a82cdd14679ed36e3395c6cfa6c1316d992c48
Carolyn Evans <carolyn.evans@enom.com>
2015-05-06 17:25:13 -0700

    Added affiliate api result xml shredding and mysql database update.

--- ad_mania.py/database_update.py
+++ ad_mania.py/database_update.py
@@ -1,8 +1,10 @@
 from config import Config
 import logging
 import xml.etree.ElementTree as ET
-import mysql.connector
+from mysql.connector import MySQLConnection, Error
 import requests
+from codecs import encode
+from lxml import html
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +24,9 @@ def database_update():
     cjauth = config['CJ_AUTH']
     cjurl = config['CJ_URL']
 
-    conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
+    conn = MySQLConnection(user=user, password=password, host=host, database=database)
+    conn.autocommit = True
+    cursor = conn.cursor()
 
     page_number = 0
     records_per_page = 100 # this is the max number allowed by the affiliate api per call.
@@ -33,19 +37,34 @@ def database_update():
         page_number += 1
         params = {'website-id': '7782886', 'link-type': 'banner', 'advertiser-ids': 'joined', 'page-number': page_number, 'records-per-page': records_per_page}
 
-        result_xml = requests.get(cjurl, headers=headers, params=params)
+        result = requests.get(cjurl, headers=headers, params=params)
+        result_xml = result.text
 
-        root = ET.fromstring(result_xml)
-        records_returned = root.find('links').get('records-returned')
+        root = ET.fromstring(result_xml.encode('utf8'))
+        records_returned = int(root.find('links').get('records-returned'))
 
         for link in root.iter('link'):
+            link_code_html = html.fromstring(link.find('link-code-html').text)
+            height = int(link_code_html.xpath('//img/@height')[0])
+            width = int(link_code_html.xpath('//img/@height')[0])
+
             mysql_args = (
-                link.find('advertiser-id').text,
                 link.find('link-id').text,
-                link.find('promotion-start-date').text,
-                link.find('promotion-end-date').text,
+                link.find('advertiser-id').text,
+                'None' if link.find('promotion-start-date').text == None else link.find('promotion-start-date').text,
+                'None' if link.find('promotion-end-date').text == None else link.find('promotion-end-date').text,
+                height,
+                width,
                 link.find('link-code-html').text)
 
+            try:
+                cursor.callproc('AdMania.prc_UpdateAd',mysql_args)
+
+            except Error as e:
+                print e
+
+    cursor.close()
+    conn.close()
 
 
 if __name__ == '__main__':
--- database/Procedures/prc_UpdateAd.sql
+++ database/Procedures/prc_UpdateAd.sql
@@ -1,14 +1,21 @@
 DELIMITER ;;
-
 DROP PROCEDURE IF EXISTS prc_UpdateAd;;
 
 CREATE PROCEDURE prc_UpdateAd (
 	paramAdID INT, 
 	paramAdvertiserID INT, 
-	paramStartDate datetime, 
-	paramEndDate datetime, 
+	paramStartDate varchar(50), 
+	paramEndDate varchar(50), 
 	paramHTML varchar(2000))
 BEGIN
+	DECLARE a,b INT;
+	IF paramStartDate = 'None' THEN SET paramStartDate = NULL; END IF;
+	IF paramEndDate = 'None' THEN SET paramEndDate = NULL; END IF;
+
+set a = cast(rand() * 100000 as unsigned);
+set b = cast(rand() * 100000 as unsigned);
+INSERT Ad (AdID, AdvertiserID, StartDate, EndDate, HTML)
+VALUES (a,b,'2010-01-01','2020-01-01','HTML');
 	IF EXISTS (SELECT 1	FROM Ad WHERE AdID = paramAdID AND AdvertiserID = paramAdvertiserID LIMIT 1) THEN
 		UPDATE Ad
 		SET StartDate = coalesce(paramStartDate, '1900-01-01'),
@@ -30,4 +37,10 @@ BEGIN
 			paramHTML);
 	END IF;
 END;;
+
+GRANT EXECUTE ON PROCEDURE AdMania.prc_UpdateAd to 'adservicer'@'%';;
+
+call prc_UpdateAd(123213,3213213,'None','None','html');
+select * from Ad;
+
 
