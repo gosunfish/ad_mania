968ccb7a577645317b5d3972f36d9a2560dbb611
unknown <gosunfish@comcast.net>
2015-05-09 09:44:38 -0700

    Add category to database.

--- database/Procedures/prc_UpdateAd.sql
+++ database/Procedures/prc_UpdateAd.sql
@@ -1,46 +1,51 @@
-DELIMITER ;;
-DROP PROCEDURE IF EXISTS prc_UpdateAd;;
+DROP PROCEDURE IF EXISTS prc_UpdateAd;
 
-CREATE PROCEDURE prc_UpdateAd (
+CREATE PROCEDURE AdMania.`prc_UpdateAd`(
 	paramAdID INT, 
 	paramAdvertiserID INT, 
+  paramAdvertiserName varchar(50),
+  paramCategory varchar(50),
 	paramStartDate varchar(50), 
-	paramEndDate varchar(50), 
+	paramEndDate varchar(50),
+  paramHeight int,
+  paramWidth int,
 	paramHTML varchar(2000))
 BEGIN
-	DECLARE a,b INT;
 	IF paramStartDate = 'None' THEN SET paramStartDate = NULL; END IF;
 	IF paramEndDate = 'None' THEN SET paramEndDate = NULL; END IF;
 
-set a = cast(rand() * 100000 as unsigned);
-set b = cast(rand() * 100000 as unsigned);
-INSERT Ad (AdID, AdvertiserID, StartDate, EndDate, HTML)
-VALUES (a,b,'2010-01-01','2020-01-01','HTML');
 	IF EXISTS (SELECT 1	FROM Ad WHERE AdID = paramAdID AND AdvertiserID = paramAdvertiserID LIMIT 1) THEN
 		UPDATE Ad
-		SET StartDate = coalesce(paramStartDate, '1900-01-01'),
-			EndDate = coalesce(paramEndDate, '2999-12-31'),
-			HTML = paramHTML
+		SET AdvertiserName = paramAdvertiserName,
+        Category = paramCategory,
+        Height = paramHeight,
+        Width = paramWidth,
+        StartDate = coalesce(paramStartDate, '1900-01-01'),
+			  EndDate = coalesce(paramEndDate, '2999-12-31'),
+			  HTML = paramHTML
 		WHERE AdID = paramAdID AND AdvertiserID = paramAdvertiserID;
 	ELSE
 		INSERT Ad (
 			AdID, 
 			AdvertiserID, 
+      AdvertiserName,
+      Category,
 			StartDate, 
 			EndDate, 
+      Height,
+      Width,
 			HTML)
 		VALUES (
 			paramAdID, 
 			paramAdvertiserID, 
+      paramAdvertiserName,
+      paramCategory,
 			coalesce(paramStartDate, '1900-01-01'), 
 			coalesce(paramEndDate, '2999-12-31'),
+      paramHeight,
+      paramWidth,
 			paramHTML);
 	END IF;
-END;;
+END;
 
 GRANT EXECUTE ON PROCEDURE AdMania.prc_UpdateAd to 'adservicer'@'%';;
-
-call prc_UpdateAd(123213,3213213,'None','None','html');
-select * from Ad;
-
-
--- database/Tables/Ad.sql
+++ database/Tables/Ad.sql
@@ -4,6 +4,7 @@ CREATE TABLE Ad (
 	ID int auto_increment not NULL primary key,
 	AdID int not NULL,
 	AdvertiserID int not NULL,
+  Category varchar(50) not NULL default '',
 	StartDate datetime not NULL default '1900-01-01',
 	EndDate datetime not NULL default '2999-12-31',
 	HTML varchar(2000) not NULL,
